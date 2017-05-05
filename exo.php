435885fa9a60c1c0f52e6804238598d6c7955abe
ddvdv <vdv.david@gmail.com>
2017-05-05 10:17:49 +0200

    Exo dans le cadre de hackersPoulette

---
+++ exo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+
+	<body>
+
+	<?php
+
+	$prenom = 'David';
+	$age = 28;
+	$faim = true;
+	$eyes = "brown";
+	$famille = array['David', 'AnneSophie', 'Félix'];
+
+	echo $prenom;
+	echo $age;
+	echo $faim;
+	echo $eyes; 
+	echo $famille;
+
+
+	?>
+
+	</body>
+
+
+</html>
